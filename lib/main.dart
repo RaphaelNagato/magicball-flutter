c699a5545996575bb7f2d6b9e57c4e91802f9acf
Raphael Okeibunor <raphaelnagato19@gmail.com>
2020-10-07 11:19:42 +0100

    magic-8-ball-flutter up and running

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.green,
+          appBar: AppBar(
+            title: Text("Ask me anything"),
+            backgroundColor: Colors.green[800],
+          ),
+          body: Container(
+            child: MagicBall(),
+          ),
+        ),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballValue = 1;
+
+  void magicBallAnswer() {
+    setState(() {
+      ballValue = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () => magicBallAnswer(),
+              child: Image.asset('images/ball$ballValue.png'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
